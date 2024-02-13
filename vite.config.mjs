6cf426a5445f80e31b559bddc5c9c2ee95296fd0
aniket jain <aniket1995jain@gmail.com>
2024-02-13 19:57:29 +0530

    init: initial setup with React Vite TypeScript & Mantine library

---
+++ vite.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	// depending on your application, base can also be "/"
+	base: process.env.VITE_APP_BASE_NAME || "/",
+	plugins: [react(), viteTsconfigPaths()],
+	define: {
+		global: "window",
+	},
+	resolve: {
+		alias: [],
+	},
+	server: {
+		// this ensures that the browser opens upon server start
+		open: true,
+		// this sets a default port to 3000
+		port: 3000,
+	},
+});
